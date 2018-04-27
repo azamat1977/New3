dabc72aad07cb9bd12ba0b768696311eba5c943a
azamat1977 <aza.azem@gmail.com>
2018-04-27 19:16:42 +0600

    New commit

---
+++ 23.py
@@ -0,0 +1,11 @@
+x=int(input('Enter age: '))
+42
+if x<0:
+    x=0
+    print ('Отрицательное значение изменено на ноль')
+elif x==0:
+    print ('Zero')
+elif x==1:
+    print ('One')
+else:
+    print('More')
